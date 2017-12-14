99ba06bfaca5ba8acdeecf2169d54c3ea0ae6283
rong3132003 <185775374@qq.com>
2017-12-14 16:30:12 +0800

    7
    7

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -12,9 +12,9 @@ import java.sql.ResultSetMetaData;
 import java.util.*;
 import java.text.SimpleDateFormat;
 import java.io.InputStream;
-//import ch.ethz.ssh2.ChannelCondition;
-//import ch.ethz.ssh2.Session;
-//import ch.ethz.ssh2.StreamGobbler;
+import ch.ethz.ssh2.ChannelCondition;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 import java.security.MessageDigest;
 import java.net.*;
 import java.util.regex.*;
@@ -22,11 +22,92 @@ import java.util.regex.*;
 public class AutoCheck{
   public static void main(String[] args )
   {
-        System.out.println("hello autocheck");
+        String a = rmt_shell("192.168.197.151","root","root123","df -h");
+        System.out.println(a);
   }
 
   public static void test1()
   {
         System.out.println("this is a test!");
   }
+
+  //远程调用shell
+    public static String rmt_shell(String ip, String username, String password, String cmd)
+    {
+        String re = null;
+        String line = null;
+         try
+        {
+            /* Create a connection instance */
+            //因为连接db已经有import java.sql.Connection，所以这里不能import ch.ethz.ssh2.Connection，要换个引用方法；
+            ch.ethz.ssh2.Connection conn = new ch.ethz.ssh2.Connection(ip, 22);
+
+            /* Now connect */
+
+            conn.connect();
+
+            /* Authenticate.
+             * If you get an IOException saying something like
+             * "Authentication method password not supported by the server at this stage."
+             * then please check the FAQ.
+             */
+
+            boolean isAuthenticated = conn.authenticateWithPassword(username, password);
+
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+
+            /* Create a session */
+
+            Session sess = conn.openSession();
+
+            sess.execCommand(cmd);
+
+            /*
+             * This basic example does not handle stderr, which is sometimes dangerous
+             * (please read the FAQ).
+             */
+
+            InputStream stdout = new StreamGobbler(sess.getStdout());
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+
+            while (true)
+            {
+                line = br.readLine();
+
+                if (line == null)
+                    break;
+
+                if(re == null)
+                {
+                    re = line + "\r";
+                }
+                else
+                {
+                    re = re + line + "\r";
+                }
+            }
+
+            /* Show exit status, if available (otherwise "null") */
+
+            //System.out.println("ExitCode: " + sess.getExitStatus());
+
+            /* Close this session */
+
+            sess.close();
+
+            /* Close the connection */
+
+            conn.close();
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace(System.err);
+            System.exit(2);
+        }
+        //System.out.println(re);
+        return re;
+    }
 }
