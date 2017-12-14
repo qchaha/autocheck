a5e65c58c61ba282666a8649c9cd4303ef4ef553
rong3132003 <185775374@qq.com>
2017-12-14 15:23:27 +0800

    5
    5

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -1,4 +1,23 @@
 package maven.autocheck;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSetMetaData;
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.io.InputStream;
+import ch.ethz.ssh2.ChannelCondition;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
+import java.security.MessageDigest;
+import java.net.*;
+import java.util.regex.*;
 
 public class AutoCheck{
   public static void main(String[] args )
@@ -8,6 +27,6 @@ public class AutoCheck{
 
   public static void test1()
   {
-        System.out.println("this is a test!");  
+        System.out.println("this is a test!");
   }
 }
