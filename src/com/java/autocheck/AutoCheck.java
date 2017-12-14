ed3e33215c1bc749e8a353e27041e63420d5d41b
rong3132003 <185775374@qq.com>
2017-12-14 13:42:50 +0800

    1
    1

---
+++ src/com/java/autocheck/AutoCheck.java
@@ -0,0 +1,8 @@
+package autocheck;
+
+public class AutoCheck{
+  public static void main(String[] args )
+  {
+        System.out.println("hello autocheck");
+  }
+}
---
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -0,0 +1,8 @@
+package maven.autocheck;
+
+public class AutoCheck{
+  public static void main(String[] args )
+  {
+        System.out.println("hello autocheck");
+  }
+}
