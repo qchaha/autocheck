cdcc4edecbe12899d2610f79907e592b2f5fa0eb
rong3132003 <185775374@qq.com>
2017-12-17 12:08:11 +0800

    1
    1

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -32,7 +32,7 @@ public class AutoCheck{
                        "echo \"echo '#<tag:free>'\" >> test.sh; echo 'free -m' >> test.sh;" +
                        "echo \"echo '#<tag:df>'\" >> test.sh; echo 'df -h' >> test.sh;" +
                        "echo \"echo '#<tag:vmstat>'\" >> test.sh; echo 'vmstat 1 5' >> test.sh;" +
-                       //"echo \"echo '#<tag:lsnrctl>'\" >> test.sh;echo 'su - oracle -c \"lsnrctl status\" >> test.sh;" +
+                       "echo \"echo '#<tag:lsnrctl>'\" >> test.sh;echo 'su - oracle -c \"lsnrctl status\"' >> test.sh;" +
                        "sh test.sh";
         String a = rmt_shell("192.168.197.113","root","root123",s_cmd);
         System.out.println(a);
