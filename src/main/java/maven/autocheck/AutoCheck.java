e3c9ee763d7c426a5c7cfd7efdb184ce61d61424
rong3132003 <185775374@qq.com>
2017-12-18 14:16:35 +0800

    2
    2

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -177,7 +177,7 @@ public class AutoCheck{
                             "echo 'exit' >> /home/oracle/dbcheck.sql;" +
                             "chmod 777 /home/oracle/dbcheck.sql;su - oracle -c \"sqlplus -S / as sysdba @/home/oracle/dbcheck.sql\"";
 
-        String s_awr = "echo 'SET ECHO OFF' >> /home/oracle/creawr.sql;" +
+        String s_awr = "echo 'SET ECHO OFF' > /home/oracle/creawr.sql;" +
                        "echo 'SET VERI OFF' >> /home/oracle/creawr.sql;" +
                        "echo 'SET FEEDBACK OFF' >> /home/oracle/creawr.sql;" +
                        "echo 'SET TERMOUT ON' >> /home/oracle/creawr.sql;" +
