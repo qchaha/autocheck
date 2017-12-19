8d2ea572c45b14f44710761501a52debafcf30c0
rong3132003 <185775374@qq.com>
2017-12-19 10:05:15 +0800

    1
    1

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -208,10 +208,10 @@ public class AutoCheck{
                        "echo 'e_num=$(echo ${b_num}+8 | bc)' >> /home/oracle/awr_statistics.sh;" +
                        "echo 'cat /home/oracle/awr.txt | sed -n \"${b_num},${e_num}p\" > /home/oracle/awr_statistics.log' >> /home/oracle/awr_statistics.sh;" +
 
-                       "echo \"b_num=$(cat /home/oracle/awr.txt  | grep -in 'SQL ordered by Elapsed Time' | awk '{print $1}' | cut -d ':' -f 1  | sed -n 1p)\" >> /home/oracle/awr_statistics.sh;";/* +
+                       "echo \"b_num=$(cat /home/oracle/awr.txt  | grep -in 'SQL ordered by Elapsed Time' | awk '{print $1}' | cut -d ':' -f 1  | sed -n 1p)\" >> /home/oracle/awr_statistics.sh;" +
                        "echo \"e_num=$(cat /home/oracle/awr.txt  | grep -in 'SQL ordered by Elapsed Time' | awk '{print $1}' | cut -d ':' -f 1  | sed -n 2p)\" >> /home/oracle/awr_statistics.sh;" +
                        "echo \"e2_num=$(cat /home/oracle/awr.txt  | grep -in 'SQL ordered by Elapsed Time' | awk '{print $1}' | cut -d ':' -f 1  | sed -n 2p)\" >> /home/oracle/awr_statistics.sh;" +
-                       "echo \"e2_num=$(echo ${e2_num}-1 | bc)\" >> /home/oracle/awr_statistics.sh;" +
+                       "echo \"e2_num=$(echo ${e2_num}-1 | bc)\" >> /home/oracle/awr_statistics.sh;";/* +
                        "echo 'cat /home/oracle/awr.txt | sed -n \"${b_num},${e_num}p\" | sed -n \"8,${e2_num}p'\" >> /home/oracle/awr_statistics.log;" +
                        "chmod +x /home/oracle/awr_statistics.sh;sh /home/oracle/awr_statistics.sh;cat /home/oracle/awr_statistics.log;"
                        ;*/
