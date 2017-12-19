fb55d2913501d50414421bca7ffbb5ac1b5e4643
rong3132003 <185775374@qq.com>
2017-12-19 11:07:31 +0800

    2
    2

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -229,6 +229,13 @@ public class AutoCheck{
 
         String a = rmt_shell("192.168.197.113","root","root123",s_oscheck + s_ins_check + s_db_check + s_awr);
         System.out.println(a);
+
+        File writename = new File("/home/oracle/output.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件
+        writename.createNewFile(); // 创建新文件
+        BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+        out.write(a); // \r\n即为换行
+        out.flush(); // 把缓存区内容压入文件
+        out.close(); // 最后记得关闭文件 
     }
 
     public static String f_date()
