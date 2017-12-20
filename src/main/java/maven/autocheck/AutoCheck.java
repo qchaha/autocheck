79d466ed636ae15272b48e860010584d5b6ff546
rong3132003 <185775374@qq.com>
2017-12-20 12:43:51 +0800

    2
    2

--- src/main/java/maven/autocheck/AutoCheck.java
+++ src/main/java/maven/autocheck/AutoCheck.java
@@ -59,8 +59,8 @@ public class AutoCheck{
          s_return = s_check_result.substring(0, i_begin);
      }
      //i_length = s_return.indexOf("#<tag:", s_return.indexOf("#<tag:") + 1);
-     i_end = i_begin + i_length;
-     s_return = s_check_result.substring(i_begin, i_end);
+     //i_end = i_begin + i_length;
+     //s_return = s_check_result.substring(i_begin, i_end);
      return s_return;
   }
 
